a040dda7feba4b7a84e12c9accbebeeacfe716c8
Rodrigo Costa <rodrigo.costa@gmail.com>
2024-05-26 01:34:02 -0300

    support multihost swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -13,7 +13,7 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
+        url: `http://${process.env.HOST}:${process.env.PORT}/api`,
       },
     ],
   },
